4979eee87dcc08447849e7592734d4524045e062
fabianh001 <fabian.h001@gmail.com>
2018-07-16 10:54:42 +0200

    Create Readme.ml

---
+++ Readme.ml
@@ -0,0 +1,6 @@
+Check out the slides!
+
+This is a guide about how to set up your workspace with Metamask, Remix and the Robsten Test Network and build your own Smart Contract with these tools.
+You can also have a look on our example voting Smart Contract
+
+Have fun!
